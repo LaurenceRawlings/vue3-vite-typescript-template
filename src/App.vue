ad6bdd32be5da6e1140dc7e8d6d587b52e8d3a33
Laurence Rawlings <rawlingslaurence@gmail.com>
2020-11-12 12:24:17 +0000

    Update template page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+<div class="flex h-screen items-center justify-center">
+    <div class="p-10 flex flex-col items-center justify-center">
+        <img class="mb-5" alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Vue 3.0 + Vite + TypeScript" />
+    </div>
+</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,10 @@
 <template>
-<h1>{{ msg }}</h1>
-<button @click="count++">count is: {{ count }}</button>
-<p>
-    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+<h1 class="text-green-500 text-3xl font-bold">{{ msg }}</h1>
+<button class="m-5 py-2 px-4 rounded bg-green-500 hover:bg-green-400 text-white transition duration-200 ease-out" @click="count++">
+    Counter: {{ count }}
+</button>
+<p class="text-center">
+    Edit <code class="font-bold">components/HelloWorld.vue</code> <br>to test hot module replacement.
 </p>
 </template>
 
